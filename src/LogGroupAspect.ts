574d2970ec5855178ed32ab7642af80fdba272bf
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2019-11-13 19:34:37 +0900

    Add removalPolicy to LogGroupAspectProps

--- src/LogGroupAspect.ts
+++ src/LogGroupAspect.ts
@@ -1,16 +1,19 @@
-import { IAspect, IConstruct, Resource } from '@aws-cdk/core';
+import { IAspect, IConstruct, Resource, RemovalPolicy } from '@aws-cdk/core';
 import { LogGroup, RetentionDays } from '@aws-cdk/aws-logs';
 import { isFunction } from './Function';
 import { isProject } from './Project';
 
 interface LogGroupAspectProps {
+  removalPolicy?: RemovalPolicy;
   retention?: RetentionDays;
 }
 
 export class LogGroupAspect implements IAspect {
+  private readonly removalPolicy?: RemovalPolicy;
   private readonly retention?: RetentionDays;
 
   constructor(props: LogGroupAspectProps = {}) {
+    this.removalPolicy = props.removalPolicy;
     this.retention = props.retention;
   }
 
@@ -24,11 +27,13 @@ export class LogGroupAspect implements IAspect {
     if (isFunction(node)) {
       new LogGroup(stack, `${id}LogGroup`, {
         logGroupName: `/aws/lambda/${node.functionName}`,
+        removalPolicy: this.removalPolicy,
         retention: this.retention,
       });
     } else if (isProject(node)) {
       new LogGroup(stack, `${id}LogGroup`, {
         logGroupName: `/aws/codebuild/${node.projectName}`,
+        removalPolicy: this.removalPolicy,
         retention: this.retention,
       });
     }
