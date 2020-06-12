b4d5ef9b7fbdc711338f8aad1a7562fbe62dba3f
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2020-06-12 21:20:25 +0900

    Add createResolvers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './LogGroupAspect';
 export * from './PermissionsBoundaryAspect';
+export * from './createResolvers';
 export * from './grantActions';
 export * from './grantStack';
 export * from './grantSynth';
