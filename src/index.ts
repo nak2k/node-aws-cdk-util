503c90c5ca366cda0fdd5ca4044aa1970d8ac59e
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2020-08-25 23:28:19 +0900

    Remove @aws-cdk/aws-appsync

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export * from './DefaultEnvStack';
 export * from './LogGroupAspect';
 export * from './NodejsProject';
 export * from './PermissionsBoundaryAspect';
-export * from './createResolvers';
 export * from './grantActions';
 export * from './grantStack';
 export * from './grantSynth';
