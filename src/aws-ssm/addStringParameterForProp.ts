c05b077c9e148c32564874b00dce37ac2cabef96
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2023-05-12 17:45:47 +0900

    Support multiple props

--- src/aws-ssm/addStringParameterForProp.ts
+++ src/aws-ssm/addStringParameterForProp.ts
@@ -3,7 +3,7 @@ import { StringParameter } from "aws-cdk-lib/aws-ssm";
 import { stringParameterNameForProp } from "./stringParameterNameForProp";
 
 export function addStringParameterForProp<T extends IResource>(resource: T, propName: keyof T) {
-  return new StringParameter(resource, "StringParameter", {
+  return new StringParameter(resource, `StringParameter.${String(propName)}`, {
     parameterName: stringParameterNameForProp<T>(resource, propName),
     stringValue: String(resource[propName]),
   });
