8950c3ffd686356eb01b2c35e296612a167d9a47
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2024-02-20 23:24:41 +0900

    Update stringParameterNameForProp to support CfnResource

--- src/aws-ssm/stringParameterNameForProp.ts
+++ src/aws-ssm/stringParameterNameForProp.ts
@@ -1,5 +1,5 @@
-import { IResource } from "aws-cdk-lib";
+import { CfnResource, IResource } from "aws-cdk-lib";
 
-export function stringParameterNameForProp<T extends IResource>(resource: T, propName: keyof T) {
+export function stringParameterNameForProp<T extends IResource | CfnResource>(resource: T, propName: keyof T) {
   return `/${resource.stack.stackName}/${resource.node.id}/${String(propName)}`;
 }
